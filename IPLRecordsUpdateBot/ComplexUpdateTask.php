b287ada8b334e560320f29e67cad1df1952810eb
jfd34 <jfd34@unknownserver.fake>
2013-10-05 22:42:06 +0530

    Add MIT license blocks and delete unnecessary file

--- IPLRecordsUpdateBot/ComplexUpdateTask.php
+++ IPLRecordsUpdateBot/ComplexUpdateTask.php
@@ -1,5 +1,29 @@
 <?php
 
+/*
+    The MIT License (MIT)
+
+    Copyright (c) 2013 "jfd34"
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+*/
+
 /**
  * An update task which is used when more than one table must be parsed on a single page, so that the
  * page does not have to be reloaded over and over again. This can be used when a single page of data should be used
